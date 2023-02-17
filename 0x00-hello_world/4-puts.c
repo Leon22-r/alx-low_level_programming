0b9f38314251eae75604e25df992e1eb588e2872
Leon <njihialeon00@gmail.com>
2023-02-17 12:17:58 -0800

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - starting point of the prograam
+ */
+void main(void)
+{
+	long mine[] = "Programming is like building a multilingual puzzle";
+
+	puts(mine);
+
+	return (0);
+}
