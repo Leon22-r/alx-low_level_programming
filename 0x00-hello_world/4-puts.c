c6d830b5a0d81f34dc553ea7d9138a644837078e
Leon <njihialeon00@gmail.com>
2023-02-17 12:30:27 -0800

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - starting point of the prograam
+ * mine - string to be printed
  */
 int main(void)
 {
-	char mine[] = "Programming is like building a multilingual puzzle";
+	char mine[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(mine);
 
