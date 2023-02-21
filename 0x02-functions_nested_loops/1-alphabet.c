81ceb0807114a5fea3460a80deb16e7d834539c4
Leon <njihialeon00@gmail.com>
2023-02-21 14:35:47 -0800

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,13 @@
  *
  * Return: always 0
  */
-int main(void)
+void print_alphabet(void)
 {
-	printf("abcdefghijklmnopqrstuvwxyz\n");
+	printf("abcdefghijklmnopqrstubwxyz");
+	return;
+}
+int main (void)
+{
+	print_alphabet();
 	return (0);
 }
