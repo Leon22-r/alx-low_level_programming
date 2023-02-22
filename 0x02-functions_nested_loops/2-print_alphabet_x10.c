cec27cee9251e1d80923a8d6ee96b9d33a58d710
Leon <njihialeon00@gmail.com>
2023-02-22 03:49:58 -0800

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Entry point
+ * Return: void
+ */
+
+void print_alphabet_x10(void);
+{
+	int i = 0;
+	char letters;
+
+	if (i <= 9)
+	{
+		for (letters = 'a'; letters <= 'z'; letters++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
