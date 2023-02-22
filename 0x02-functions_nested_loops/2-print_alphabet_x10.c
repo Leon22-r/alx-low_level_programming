4ce37b0bc0e583483bda631fe9165cfa77c377ed
Leon <njihialeon00@gmail.com>
2023-02-22 11:58:41 -0800

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,17 @@
 void print_alphabet_x10(void)
 {
 	int i = 0;
-	char letters;
+	char s;
 
 	if (i <= 9)
 	{
-		for (letters = 'a'; letters <= 'z'; letters++)
+		for (s = 'a'; s <= 'z'; s++)
 		{
 			_putchar(c);
 		}
+
 		_putchar('\n');
+
 		i++;
 	}
 }
