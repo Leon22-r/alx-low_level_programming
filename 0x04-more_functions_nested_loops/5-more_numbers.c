4079ce8e58db16f3521831c8f890950b035fa2fa
Leon <njihialeon00@gmail.com>
2023-02-24 03:05:39 -0800

    new

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - prints numbers
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	char c;
+	int i;
+
+	for (i == 0; i <= 10; i++)
+	{
+		for (c == '0'; c <= '14'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+	}
+}
