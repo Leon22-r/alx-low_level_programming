02d3da70fc7abdbaf6c3dbbe07bc520a199f7b04
Leon <njihialeon00@gmail.com>
2023-02-24 01:49:55 -0800

    new

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - entry point of the code
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('num');
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,4 +6,5 @@ int _isdigit(int c);
 
 int mul(int a,int b);
 
+int print_numbers(void);
 
