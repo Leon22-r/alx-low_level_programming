a33b8857e235e6ff32bcd0b201b097c967a775f1
Leon <njihialeon00@gmail.com>
2023-02-24 09:49:47 -0800

    new

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - prints a line on the terminal
+ * @n: number of dash line
+ */
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
