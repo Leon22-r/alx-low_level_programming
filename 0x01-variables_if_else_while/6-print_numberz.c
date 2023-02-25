8885e8e3e0e44b12b908978dd48d93fec481d66f
Leon <njihialeon00@gmail.com>
2023-02-25 06:33:10 -0800

    new

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers using putchar
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
