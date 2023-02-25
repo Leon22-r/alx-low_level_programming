345110bc5310a4dc670232d421a1304d9829db70
Leon <njihialeon00@gmail.com>
2023-02-25 09:23:08 -0800

    new

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,16 @@
 #include "main.h"
 /**
  * _isalpha - entry point
- * Descrption:
  * @c: received value
  * Return: 1 if true and 0 if false
  */
 
 int _isalpha(int c)
 {
-	int try = 'a'
-
-	for (try = 'a'; try <= 'Z'; ++try)
-	{
-		if (c == try)
-		{
-			return (1);
-		}
-	return (0);
-	}
+	if (c >= 'a' || c <= 'z')
+		return (1);
+	else if (c >= 'A' || c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
