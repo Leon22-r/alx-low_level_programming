b26cf6b5e0255049b09d95e5b4b764a34d939d45
Leon <njihialeon00@gmail.com>
2023-02-25 04:10:45 -0800

    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print letters except for q and e
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
