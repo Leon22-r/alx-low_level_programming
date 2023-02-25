33289f6310bcd57601b4aa7ebb3ec375d483e536
Leon <njihialeon00@gmail.com>
2023-02-25 09:58:12 -0800

    new

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints a sign
+ * @n: input
+ * Return: 0 or 1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n == 0);
+	{
+		printf("0");
+		return (0);
+	}
+	else
+	{
+		printf("-");
+		return (-1);
+	}
+}
