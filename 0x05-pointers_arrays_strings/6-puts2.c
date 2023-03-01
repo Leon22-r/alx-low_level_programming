947cc2b09f5d1480f586c845f94df65342ca709a
Leon <njihialeon00@gmail.com>
2023-03-01 11:18:40 -0800

    new

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * puts2 - alternates while printing by skipping every other character
+ * @str - string's pointer
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int index;
+
+	for (index = 0; index < _strlen(str); index++)
+	{
+		if (index % 2 == 0)
+		{
+			_putchar(str[index]);
+		}
+	}
+	_putchar('\n');
+}
