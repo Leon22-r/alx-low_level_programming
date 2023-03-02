af6a252861673531a17e180be8e3489607c04d3e
Leon <njihialeon00@gmail.com>
2023-03-02 00:27:22 -0800

    new

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,9 @@
  * @s1: a pointer to the first sring to be compared
  * @s2: a pointer to the second string to be compared
  *
- * Return: if str1 < str2 the negative difference of the first unmatched character
- * 	if s1 == s2 , 0
- * 	if  s2 < s1 , the positive difference of the first unmatched character
+ * Return: if str1 < str2 the negative diff of the first unmatched character
+ *	if s1 == s2 , 0
+ *	if  s2 < s1 , the positive difference of the first unmatched character
  */
 int _strcmp(char *s1, char *s2)
 {
