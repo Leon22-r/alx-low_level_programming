55d728ed1cdd7f3cdae1f364d3889c64b9d43b2b
Leon <njihialeon00@gmail.com>
2023-03-03 04:42:10 -0800

    new

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x == 15 || x == 30 || x == 60 || x == 45 || x == 75 || x == 90)
+		if (x % 3 == 0 && (x % 5) == 0)
 		{
 			printf("FizzBuzz ");
 		}
