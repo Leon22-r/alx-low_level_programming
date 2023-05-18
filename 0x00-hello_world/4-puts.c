787b33f866c121d023969d935b37af562810e912
leon Njihia <njihialeon00@gmail.com>
2023-05-18 10:26:27 -0700

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - using puts
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char name[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(name);
+	return (0);
+}
