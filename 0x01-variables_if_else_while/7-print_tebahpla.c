0af33693ab94431245401d27baee4accbe8f5eb9
leon Njihia <njihialeon00@gmail.com>
2023-05-22 11:08:52 -0700

    new

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in reverse.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
