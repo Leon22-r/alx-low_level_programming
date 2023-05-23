0e3fee63a11d1aaaf4ed794ef103c110d32045a2
leon Njihia <njihialeon00@gmail.com>
2023-05-23 04:33:19 -0700

    new

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - determine whether is a lowercasse letter.
+ *
+ * Return: 0.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
