7835f422fe28c4d2eb02298ee550dbc7a83551c8
leon Njihia <njihialeon00@gmail.com>
2023-05-23 05:35:24 -0700

    new

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns the last digit.
+ * @x: the value
+ *
+ * Return: the last digit.
+ *
+ */
+int print_last_digit(int x)
+{
+	return (x % 10);
+}
