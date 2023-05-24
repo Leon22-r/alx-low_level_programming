57d2c6a8294819cb11ce58a4c5a480059ae23a4c
leon Njihia <njihialeon00@gmail.com>
2023-05-24 09:33:25 -0700

    new

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,8 @@ void print_to_98(int n)
 {
 	int x;
 
-	if (n < 98 && n >= 0)
+	x = n;
+	if (n <= 98 && n >= 0)
 	{
 		for (x = n; x <= 98; x++)
 		{
@@ -21,20 +22,44 @@ void print_to_98(int n)
 				_putchar(',');
 				_putchar(' ');
 			}
-			else
+			else if
 			{
 				_putchar('0' + x / 10);
 				_putchar('0' + x % 10);
 				_putchar(',');
 				_putchar(' ');
 			}
+			else if (x == 98)
+			{
+				_putchar('0' + x / 10);
+				_putchar('0' + x % 10);
+			}
 		}
 	}
 	else if (n > 98)
 	{
+		more_than(x);
+	}
+	else if (n < 98)
+	{
+		less_than(x);
+	}
+}
+
+/**
+ * more_than - prints numbers more than 98.
+ * @n: the number
+ *
+ * Return: void.
+ */
+
+void more_than(int n)
+{
+	int x;
+
 		for (x = n; x >= 98; x--)
 		{
-			if (x < 100)
+			if (x < 100 && x != 98)
 			{
 				_putchar('0' + x / 10);
 				_putchar('0' + x % 10);
@@ -49,16 +74,63 @@ void print_to_98(int n)
 				_putchar(',');
 				_putchar(' ');
 			}
+			else if (x == 98)
+			{
+				_putchar('0' + x / 10);
+				_putchar('0' + x % 10);
+			}
+		}
+}
+/**
+ * less_than - prints numbers less than 98.
+ * @n: first number.
+ *
+ * Return: void.
+ */
+void less_than(int n)
+{
+	int x;
+
+	x = n * -1;
+	if (x < 100)
+	{
+		for (x = n; x > 0; x--)
+		{
+			if (x >= 10)
+			{
+				_putchar('-');
+				_putchar('0' + x / 10);
+				_putchar('0' + x % 10);
+				_putchar(',');
+				_putchar(' ');
+			}
 			else
 			{
-				_putchar('0' + x / 1000);
-				_putchar('0' + x / 100);
-				_putchar('0' + x / 10);
 				_putchar('0' + x % 10);
 				_putchar(',');
 				_putchar(' ');
 			}
 		}
 	}
+	for (x = 0; x <= 98; x++)
+	{
+		if (x < 10)
+		{
+			_putchar('0' + x);
+			_putchar(',');
+			_putchar(' ');
+		}
+		else if (x >= 10 && x != 98)
+		{
+			_putchar('0' + x / 10);
+			_putchar('0' + x % 10);
+			_putchar(',');
+			_putchar(' ');
+		}
+		else if (x == 98)
+		{
+			_putchar('0' + x / 10);
+			_putchar('0' + x % 10);
+		}
+	}
 }
-
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -21,3 +21,8 @@ void times_table(void);
 int add(int, int);
 
 void print_to_98(int n);
+
+void more_than(int x);
+
+void less_than(int x);
+
