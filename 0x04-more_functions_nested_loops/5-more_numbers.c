83292c0c09fd78045c80be47cd1aaade2807c72e
leon Njihia <njihialeon00@gmail.com>
2023-05-25 02:20:49 -0700

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,6 +15,7 @@ void more_numbers(void)
 		{
 			if (i >= 10)
 				_putchar('0' + i / 10);
+
 			_putchar('0' + i % 10);
 		}
 	_putchar('\n');
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,9 +11,15 @@ void print_line(int n)
 	int x;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
+	{
 		for (x = 0; x < n; x++)
+		{
 			_putchar('_');
+		}
 		_putchar('\n');
+	}
 }
