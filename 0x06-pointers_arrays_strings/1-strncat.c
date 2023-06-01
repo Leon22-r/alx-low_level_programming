298a8fb768648f9567568a49f9cc2e1d65bf3239
leon Njihia <njihialeon00@gmail.com>
2023-06-01 01:06:49 -0700

    new

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - appends src to dest upto to a specified number.
+ * @dest: destinaton.
+ * @src: source.
+ * @n: number to reach.
+ *
+ * Return: resultant string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x;
+	int y;
+
+	y = 1;
+	for (x = 0; dest[x] != '\0'; x++)
+	{
+		y++;
+	}
+	for (x = 0; x < n; x++)
+	{
+		dest[y] = src[x];
+		y++;
+	}
+
+	return (dest);
+}
