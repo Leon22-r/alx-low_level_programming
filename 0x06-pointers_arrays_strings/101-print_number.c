8a248e506833d94bcaa4badb40f137cc5f40953d
leon Njihia <njihialeon00@gmail.com>
2023-06-02 11:54:30 -0700

    new

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+void less_than(int n);
 /**
  * print_number - prints numbers using _putchar.
  * @n: number to print.
@@ -36,8 +37,16 @@ void print_number(int n)
 		}
 		_putchar('\n');
 	}
-	if (n < 0)
-	{
+	else
+		less_than(n);
+}
+/**
+ * less_than - print numbers less than 0.
+ * @n: number.
+ * Return: void.
+ */
+void less_than(int n)
+{
 		n = n * -1;
 		_putchar('-');
 		if (n < 10)
