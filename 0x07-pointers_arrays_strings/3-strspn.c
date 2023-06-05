13d4bfde220c42b81254c67d5e2508525caa02ee
leon Njihia <njihialeon00@gmail.com>
2023-06-05 10:15:33 -0700

    new

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - calculates the length in bytes the initial of the intial segments of s
+ * @s: points to the string to search.
+ * @accept: string containing bytes to match
+ * Return: length of matching bytes.
+ */
+char *_strspn(char *s, char *accept)
+{
+	int i, count, match;
+
+	count = 0;
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				count++;
+				match = 1;
+			}
+		}
+		if (!match)
+			return (count);
+		match = 0;
+		s++;
+	}
+	return (count);
+}
