f86fca2d787c4ba670023bbda0cd20f52cf9e846
leon Njihia <njihialeon00@gmail.com>
2023-06-05 04:20:21 -0700

    new

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _memset - fills first n bytes pointed by s constant byte b.
+ * @s: memory pointer.
+ * @b: the constant byte.
+ * @n: last byte.
+ *
+ * Return: memory location pointed to by s.
+ *
+ */
+char * _memset(char *s, char b, unsigned int n)
+{}
