c1a76666d5edf6b1ed36a517e548fc6adb802699
leon Njihia <njihialeon00@gmail.com>
2023-06-05 10:31:55 -0700

    new

--- 0x07-pointers_arrays_strings/4=strpbrk.c
+++ 0x07-pointers_arrays_strings/4=strpbrk.c
@@ -2,12 +2,13 @@
 /**
  * _strpbrk - points to first element of s that matches one in accept.
  * @s: string to search match.
- * @match: charaters to search for.
+ * @accept: charaters to search for.
  * Return: pointer to first matching character.
  */
 char _strpbrk(char *s, char *accept)
 {
 	int i;
+
 	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
