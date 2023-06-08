5a9447140d4792b1a7e81b30fd793221f6fdf095
leon Njihia <njihialeon00@gmail.com>
2023-06-08 04:14:53 -0700

    new

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * is_prime_number - determines if a number is prime.
+ * @n: the number.
+ * Return: 1 if prime and 0 otherwise.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (recursion(n, 2));
+}
+/**
+ * recursion - recursively determines a prime number.
+ * @x: the number.
+ * @y: used to determine.
+ * Return: 1 if prime and 0 otherwise.
+ */
+int recursion(int x, int y)
+{
+	if ((y * y) > x)
+		return (0);
+	if (x % y == 0)
+		return (1);
+	return(recursion(x, y + 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,3 +9,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 
 int _pow_recursion(int x, int y);
+
+int is_prime_number(int n);
+
+int recursion(int x, int y);
