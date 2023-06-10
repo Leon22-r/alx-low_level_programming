7982d55948f2f35166361dd5acf20de9c4775703
leon Njihia <njihialeon00@gmail.com>
2023-06-10 01:16:18 -0700

    new

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - write's the program's name.
  * @argc: the number of arguements.
@@ -8,14 +9,7 @@
 int main(int argc, char *agrv[])
 {
 	(void) agrc;
-	(void) argv;
 
-	int i;
-
-	for (i = 0; argv[0][i] != '\0'; i++)
-	{
-		_putchar(argv[0][i]);
-	}
-	_putchar('\n');
+	printf("%s\n", argv[0]);
 	return (0);
 }
