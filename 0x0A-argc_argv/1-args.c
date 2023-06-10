e6500ce13d2640675cf8036ecd1961926508a2d8
leon Njihia <njihialeon00@gmail.com>
2023-06-10 01:23:07 -0700

    new

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguements passed.
+ * @argv: pointer to arguements passed.
+ * @argc: contains arguements passed.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d", argc - 1);
+	return (0);
+}
