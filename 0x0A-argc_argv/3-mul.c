96eb18d691679a9194336f9584d621e31f339dde
leon Njihia <njihialeon00@gmail.com>
2023-06-10 02:41:30 -0700

    new

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int atoi(char *c);
+/**
+ * main - multiplies two numbers.
+ * @argc: n.o arguements
+ * @argv: arguements.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int n, i;
+	
+	if (argc != 3)
+		return (1);
+	n = _atoi(argv[1];
+	i = _atoi(argv[2];
+	return (n * i);
+}
+/**
+ * _atoi - converts character to integers.
+ * @c: the number to change.
+ * Return: 0.
+ */
+
+int _atoi(char *c)
+{
+	int sign = 1, result = 0;
+
+	if (*c == '-')
+	{
+		sign = -1;
+		c++;
+	}
+	while (*c)
+	{
+		result = result * 10 + (*c - '0');
+		c++;
+	}
+	return (result * sign);
+}
