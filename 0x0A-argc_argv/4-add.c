4c466567e1ae99683945567818c87448652c032b
leon Njihia <njihialeon00@gmail.com>
2023-06-10 05:22:10 -0700

    new

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - adds numbers
+ * @argv: the numbers.
+ * @argc: number of numbers.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+	
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i][0] >= '0' && argv[i][0] <= '9')
+		{
+			sum += _atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			rerturn (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+int _atoi(char *c)
+{
+	int result = 0;
+
+	while (*c)
+	{
+		result = result * 10 + (*c - '0');
+		c++;
+	}
+	return (result);
+}
