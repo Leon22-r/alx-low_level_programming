2a84d6e39576023055d68638cf07c5620583585f
leon Njihia <njihialeon00@gmail.com>
2023-06-12 06:25:24 -0700

    new

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,7 +35,7 @@ char *str_concat(char *s1, char *s2)
 	f = malloc(sizeof(char) * n);
 	if (f == NULL)
 		return (NULL);
-	if (s1 == NULL && s2 == NULL)
+	if (*s1 == '\0' && *s2 == '\0')
 		return (NULL);
 	if (y != 0)
 	{
