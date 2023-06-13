dd8d6498d63f110d7172a16e9254d33761762fce
leon Njihia <njihialeon00@gmail.com>
2023-06-13 01:36:39 -0700

    new

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,6 +16,8 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i = 0; i < height; i++)
 		arr[i] = (int *)malloc(sizeof(int) * width);
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
