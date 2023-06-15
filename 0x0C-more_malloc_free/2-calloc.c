5d9f9b3d94533aa7e7f94f896532878917881e91
leon Njihia <njihialeon00@gmail.com>
2023-06-15 12:03:35 -0700

    new

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+/**
+ * _calloc - creates calloc.
+ * @nmemb: number of elements.
+ * @size: the size of each element.
+ * Return: the arrays created.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *x;
+
+	x = malloc(nmemb * sizeof(size));
+	if (x == NULL)
+		return (NULL);
+	
+	return (x);
+}
