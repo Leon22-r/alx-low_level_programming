1d8ab8a88f23d587251c21fe5d12fc9d7b959581
leon Njihia <njihialeon00@gmail.com>
2023-06-16 02:28:40 -0700

    new

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,10 +14,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	if (min == 0)
-		noarr = max;
-	else
-		noarr = (max - min) + 1;
+	noarr = (max - min) + 1;
 	array = malloc(sizeof(int) * noarr);
 	if (array == NULL)
 		return (NULL);
