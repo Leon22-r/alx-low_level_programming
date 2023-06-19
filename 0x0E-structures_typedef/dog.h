d00892ef7bb991102cdac73829c2f8a124609ce5
leon Njihia <njihialeon00@gmail.com>
2023-06-19 04:10:35 -0700

    new

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - gives the name age and owner.
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
