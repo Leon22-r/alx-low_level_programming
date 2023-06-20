834befe6fde405376224c3653837a68190484601
leon Njihia <njihialeon00@gmail.com>
2023-06-20 10:23:49 -0700

    new

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,14 +11,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name)
-		printf("Name: %s\n", d->name);
-	else
-		printf("Name: (nil)\n");
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Name: %f\n", d->age);
-	if (d->owner)
-		printf("Name: %s\n", d->owner);
-	else
-		printf("owner: (nil)\n");
-
+	printf("Name: %s\n", d->owner ? d->owner : "(nil)");
 }
