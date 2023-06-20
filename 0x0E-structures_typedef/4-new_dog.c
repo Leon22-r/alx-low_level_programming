b5e3c0b3091f1b0d3a8f9a21fec641d781f7ead6
leon Njihia <njihialeon00@gmail.com>
2023-06-19 22:41:36 -0700

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,12 +11,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
 
-	if (name && age && owner)
-	{
-		new->name = name;
-		new->age = age;
-		new->owner = owner;
-		return (new);
-	}
-	return (NULL);
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
+	return (new);
 }
