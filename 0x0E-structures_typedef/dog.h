9418f8ff137eb960022c0830a3db45731cedc333
leon Njihia <njihialeon00@gmail.com>
2023-06-20 05:58:46 -0700

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,20 @@
+#ifndef STRUCTURE
+#define STRUCTURE
+
 /**
  * struct dog - gives the name age and owner.
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner's name.
  */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }dog_t;
+#endif
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
