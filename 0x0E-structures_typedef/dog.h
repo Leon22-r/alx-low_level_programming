5fc80968a28a6347a3ca3298faf9e0dca504fb15
leon Njihia <njihialeon00@gmail.com>
2023-06-20 06:01:03 -0700

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,8 +13,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
-#endif
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
@@ -23,3 +22,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
+#endif
