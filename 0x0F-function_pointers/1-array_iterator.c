f0c0edb18621f94a45a92228846e6a824b521c57
leon Njihia <njihialeon00@gmail.com>
2023-06-21 06:08:28 -0700

    new

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+/**
+ * array_iterator - goes through each element of an array.
+ * array: the array.
+ * @size: the size of the array.
+ * @action: the function to use on the elements of the array
+ * Return: void.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (actiion)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(i);
+		}
+	}
+}
