79a8899dc4dc0922d401084385b4e3b0e7236e1e
leon Njihia <njihialeon00@gmail.com>
2023-06-21 05:32:01 -0700

    new

---
+++ 0x0F-function_pointer/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointer.h"
+/**
+ * print_name - prints a name according to function called-back
+ * @name: the name.
+ * @f: pointer to the function needed.
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointer/function_pointer.h
@@ -0,0 +1,8 @@
+#ifndef HEADER1
+#define HEADER1
+
+int _putchar(char c);
+
+void print(char *name, void (*f)(char *));
+
+#endif
