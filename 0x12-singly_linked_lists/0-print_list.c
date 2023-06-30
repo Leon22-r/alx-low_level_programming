cad3385f04ac3d9bf85979f336c2a01a2a638523
leon <njihialeon00@gmail.com>
2023-06-30 16:14:56 +0300

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t a = 1;
-	size_t x = 1;
+	size_t x = 0;
 
 	if (h != NULL)
 	{
@@ -16,7 +16,9 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 	}
-	if (h->next == NULL)
+	if (h == NULL)
 		return (x);
+	if (h->next == NULL)
+		return (a);
 	return (a + print_list(h->next));
 }
