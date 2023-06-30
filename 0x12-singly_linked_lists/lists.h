109c5623e624bf26e7114782e2dac7a452350253
leon <njihialeon00@gmail.com>
2023-06-30 15:58:38 +0300

    done

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,7 +9,7 @@
  * @str: string - (malloc'ed string)
  * @len: length of string.
  * @next: points to the next node.
- * 
+ *
  * Description: singly linked list node structure
 */
 typedef struct list_s
