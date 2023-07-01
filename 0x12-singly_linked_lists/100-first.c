1f120520d1e1f5606360f170a8426b1a585ce941
leon <njihialeon00@gmail.com>
2023-07-01 19:53:15 +0300

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,9 +2,10 @@
 /**
  * bfr_main - functions before main
  * Return: void.
- * 
+ *
 */
 void bfr_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
