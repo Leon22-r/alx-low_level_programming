26bc8cadee5025b255091cdec3f83eda22c11bbc
leon <njihialeon00@gmail.com>
2023-07-03 23:48:50 +0300

    another free function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - free in a unique way
+ * @head: a pointer to the list.
+*/
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+		return;
+	if ((*(head))->next == NULL)
+	{
+		free(*(head));
+		return;
+	}
+	free_listint2((*(head))->next);
+	free(*head);
+	head == NULL;
+}
+
