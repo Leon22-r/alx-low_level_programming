b9009e41aea230b702859b8c88b5afe1336b1d31
leon <njihialeon00@gmail.com>
2023-07-04 12:14:59 +0300

    sum of all the numbers in the list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * sum_listint - sums up all integers in the list
+ * @head: the list.
+ * Return: the sum of the list
+*/
+int sum_listint(listint_t *head)
+{
+	if (head == NULL)
+		return (0);
+	if(head->next == NULL)
+		return (head->n);
+	return (head->n + sum_listint(head->next));
+}
