e190108cf2e36a4ea9bb59a6f832c07b6ea2771e
leon <njihialeon00@gmail.com>
2023-07-05 13:47:41 +0300

    concatenating all arguemenst

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * argstostr - concatenates all the arguements
+ * @ac: the no of arguements
+ * @av: the arguements.
+ * Return: the new string
+*/
+char *argstostr(int ac, char **av)
+{
+
+	int y, c, i = 0, len = 0;
+	char *x;
+
+	if (ac <= 0 || av == NULL)
+		return (NULL);
+	for (y = 0; y < ac; y++)
+	{
+		for (c = 0; av[y][c]; c++)
+			;
+		len += c + 1;
+	}
+	x = malloc(len + 1);
+	if (x != NULL)
+	{
+		for (y = 0; y < ac; y++)
+		{
+			for (c = 0; av[y][c]; c++, i++)
+				x[i] = av[y][c];
+			x[i++] = '\n';
+		}
+		x[i] = '\0';
+	}
+	return (x);
+}	
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 int _putchar(char c);
 
 char *create_array(unsigned int size, char c);
@@ -12,3 +17,4 @@ int **alloc_grid(int width,int height);
 
 void free_grid(int **grid, int height);
 
+char *argstostr(int ac, char **av);
