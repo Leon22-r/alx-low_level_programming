7e3acf50543cf2dee99527ca06bbbdddd7b41362
leon <njihialeon00@gmail.com>
2023-07-06 22:52:38 +0300

    fixing the problem in the first task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,17 +7,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int x = 0;
-	int i;
 
-	if (b == NULL)
-		return (x);
-	while (b[i])
+	while (*b)
 	{
-		if (b[i] == '0' || b[i] == '1')
+		if (*b == '0' || *b == '1')
 		{
 			x *= 2;
-			x += b[i] - '0';
-			i++;
+			x += *b - '0';
+			b++;
 		}
 		else
 			return (0);
