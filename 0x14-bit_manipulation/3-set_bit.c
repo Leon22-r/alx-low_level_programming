2db7e9b173607772b8f70681565547944b84ba92
leon <njihialeon00@gmail.com>
2023-07-07 19:23:19 +0300

    task 4 - setting the bit at a specific index to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_bit - sets the bit at index to 1
+ * @n: the number
+ * @index: the index
+ * Return: 1 on success and -1 on failure
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit = 1;
+
+	bit = bit << index;
+	*n = *n | bit;
+	if (*n)
+		return (1);
+	else
+		return (0);
+
+}
