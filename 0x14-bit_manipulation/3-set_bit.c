a80e4892b24d43e76d54711d8193e64d0c12dc82
leon <njihialeon00@gmail.com>
2023-07-07 20:42:56 +0300

    updates

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,6 +14,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (*n)
 		return (1);
 	else
-		return (0);
+		return (-1);
 
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -15,5 +15,5 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (*n)
 		return (1);
 	else
-		return (0);
+		return (-1);
 }
