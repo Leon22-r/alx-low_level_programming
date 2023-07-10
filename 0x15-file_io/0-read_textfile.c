e2c631e072a66830775003d38265537e70c74ea6
leon <njihialeon00@gmail.com>
2023-07-10 14:56:19 +0300

    revert back

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,14 +10,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fn;
 	ssize_t prnt, readed;
-	char *letts;
+	char letts[1024];
 
 	if (filename == NULL)
 		return (0);
 
 	fn = open(filename, O_RDONLY);
-	letts = malloc(sizeof(char *));
-
 	if (fn != -1)
 	{
 		/*reading the file*/
@@ -29,6 +27,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	else
 		return (0);
-	free(letts);
 	return (prnt);
 }
