17fddff5b41a34810b4cb02d494ca1995f5e0562
leon <njihialeon00@gmail.com>
2023-07-10 15:59:18 +0300

    2nd task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - creates a new file
+ * @filename: name of file
+ * @text_content: what is to be in the file
+*/
+int create_file(const char *filename, char *text_content)
+{
+	size_t len;
+	int file;
+
+	if (filename == NULL)
+		return (-1);
+	len = strlen(text_content);
+	file = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 600);
+	if (file != 0)
+	{
+		if (text_content)
+		{
+			write(file, text_content, len);
+		}
+	}
+	else
+		return (-1);
+	return (1);
+	
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,9 +7,12 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 
+/*function prototypes*/
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
+int create_file(const char *filename, char *text_content);
 
 
 #endif
