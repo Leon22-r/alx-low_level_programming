198e5e5318184741e57121cc730469b968737000
leon <njihialeon00@gmail.com>
2023-07-10 16:06:30 +0300

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (*text_content)
+	if (text_content)
 		len = _strlen(text_content);
 	file = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 600);
 	if (file != 0)
