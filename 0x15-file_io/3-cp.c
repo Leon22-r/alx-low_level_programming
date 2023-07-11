b834b2208368de5e7ec1498b91386df0b12fb970
leon <njihialeon00@gmail.com>
2023-07-11 11:46:11 +0300

    copies content of file 1 to file 2

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * main - copies content of file 1 to file 2
+ * @av: the number of arguements
+ * @ac: the files
+ * Return: 1 on success and 0 on failure
+*/
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	int from, to, close1, close2;
+	char readed[1024];
+	ssize_t red, writen;
+	
+	if (argc != 3)
+	{
+		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	from = open(argv[1], O_RDONLY);
+	if (from < 0)
+	{
+		dprintf(STDOUT_FILENO,  "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC , 0644);
+	if (to < 0)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (to != -1 && from != -1)
+	{
+		red = read(from, readed, sizeof(readed));
+		writen =  write(to, readed, sizeof(readed));
+		close1 = close(from);
+		close2 = close(to);
+		if (close1 != 0)
+		{
+			dprintf(STDOUT_FILENO, "Error: Can't close fd %d", from);
+			exit(100);
+		}
+		if (close2 != 0)
+		{
+			dprintf(STDOUT_FILENO, "Error: Can't close fd %d", to);
+			exit(100);
+		}
+	}
+
+	
+}
