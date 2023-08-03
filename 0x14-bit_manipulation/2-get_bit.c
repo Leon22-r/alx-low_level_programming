8a73a2adb5bb923416b16d3ae3c019ae4e500afe
Leon22-r <121898166+Leon22-r@users.noreply.github.com>
2023-08-03 21:36:07 +0300

    get bit at a specific index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit - get's the bit of the specified index.
+ * @n: the number
+ * @index: the index to get the bit
+ * Return: number at specified index
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int y = 1;
+
+	y = y << index;
+	if (n & y)
+		return (1);
+	else
+		return (0);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -14,4 +14,6 @@ void print_binary(unsigned long int n);
 
 int _putchar(char c);
 
+int get_bit(unsigned long int n, unsigned int index);
+
 #endif
