91784dc56ca53741dced614af62cafb7bf69017c
Leon22-r <121898166+Leon22-r@users.noreply.github.com>
2023-08-03 14:40:06 +0300

    second task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_binary - convert a number to binary.
+ * @n: the number.
+*/
+void print_binary(unsigned long int n)
+{
+	unsigned long int bytes = sizeof(long int);
+	unsigned long int byte = 1;
+	long int zero = 1;
+
+	for (byte = byte << (bytes - 1); byte; byte = byte >> 1)
+	{
+		if (zero && !(n & byte))
+			continue;
+		if (n & byte)
+			_putchar('1');
+		else
+			_putchar('0');
+		zero = 0;
+	}
+}
