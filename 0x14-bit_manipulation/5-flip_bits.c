f9f6d4a4c6e354e342349278aa7be63581d3b63b
Leon22-r <121898166+Leon22-r@users.noreply.github.com>
2023-08-03 23:52:45 +0300

    flip task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * flip_bits - counts number of bits that change from 1 number
+ * to another
+ * @m: the first number
+ * @n: the second number
+ * Return: the number of bits that change.
+*/
+unsigned int flip_bits(unsigned long int m, unsigned long int n)
+{
+	unsigned long int bytes= sizeof(long int) * 8;
+	unsigned long int bit = 1;
+	unsigned int num = 0;
+
+	for (bit = bit << (bytes - 1); bit; bit = bit >> 1)
+	{
+		if ((m & bit) && (n & bit))
+			continue;
+		if (!(m & bit) && !(n & bit))
+			continue;
+		if ((!(m & bit) && (n & bit)) || ((m & bit) && !(n & bit)))
+			num++;
+	}
+	return (num);
+
+}
